f801fc03a3408f4e5d0d4f6d4a23504cec1c8d26
OHHAKO <l36h7129@gmail.com>
2022-08-27 00:51:22 +0900

    design: 메인페이지 디자인 개선

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import Article from './components/Article';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">React Estudio by hako</header>
-      <div className="body">test sample</div>
+      <div className="body">
+        test sample
+        <Article />
+      </div>
     </div>
   );
 }
